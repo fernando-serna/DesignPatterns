4fb4b1f64f1613a235db6a2c9f8c142e4c8dc642
Fernando Serna (Tata Consultancy Services Ltd) <v-fesern@microsoft.com>
2019-12-10 17:05:22 -0800

    added observer pattern project

---
+++ ObserverPattern/WeatherData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using static ObserverPattern.WeatherInterfaces;
+
+namespace ObserverPattern
+{
+    public class WeatherData : ISubject
+    {
+        private ArrayList observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new ArrayList();
+        }
+
+        public void registerObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void removeObserver(IObserver o)
+        {
+            int i = observers.IndexOf(o);
+            if (i >= 0)
+            {
+                observers.Remove(i);
+            }
+        }
+
+        public void notifyObservers()
+        {
+            for (int i = 0; i < observers.Count; i++) {
+                IObserver observer = (IObserver)observers.get(i);
+            }
+        }
+    }
+}
---
+++ ObserverPattern/WeatherInterfaces.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public class WeatherInterfaces
+    {
+        public interface ISubject
+        {
+            void registerObserver(IObserver o);
+            void removeObserver(IObserver o);
+            void notifyObservers();
+        }
+
+        public interface IObserver
+        {
+            public void update(float temp, float humidity, float pressure);
+        }
+
+        public interface IDisplayElement
+        {
+            void display();
+        }
+    }
+}
