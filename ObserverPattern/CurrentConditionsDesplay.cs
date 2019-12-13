cf0a422fd75e5de11c065e5b4451d845ebc576bf
Fernando Serna (Tata Consultancy Services Ltd) <v-fesern@microsoft.com>
2019-12-12 16:35:26 -0800

    added observer pattern project

---
+++ ObserverPattern/CurrentConditionsDesplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static ObserverPattern.WeatherInterfaces;
+
+namespace ObserverPattern
+{
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private ISubject weatherData;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Current conditions: {temperature}F degrees and {humidity}% humidity");
+        }
+    }
+}
--- ObserverPattern/WeatherData.cs
+++ ObserverPattern/WeatherData.cs
@@ -35,8 +35,37 @@ namespace ObserverPattern
         public void notifyObservers()
         {
             for (int i = 0; i < observers.Count; i++) {
-                IObserver observer = (IObserver)observers.get(i);
+                IObserver observer = (IObserver)observers[i];
+                observer.update(temperature, humidity, pressure);
             }
+        }
+
+        public void measurementsChanged()
+        {
+            notifyObservers();
+        }
+
+        public void setMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            measurementsChanged();
+        }
+
+        public float getTemperature()
+        {
+            return temperature;            
+        }
+
+        public float getHumidity()
+        {
+            return humidity;
+        }
+
+        public float getPressure()
+        {
+            return pressure;
         }
     }
 }
